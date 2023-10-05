9ffecc89c730ce52e09ca104cfc134d8e96943dc
dvxung <dangvanxung1412@gmail.com>
2023-10-05 15:41:02 +0700

    add file

--- src/main.java
+++ src/main.java
@@ -3,17 +3,29 @@ public class main {
     public static void main(String[] args) {
 
         int variable = 100;
+        int variable0 = 10;
         double variable2= 2.000d;
         boolean variable3= false;
 
         if (variable==100) {
-            System.out.print("true1");
+            System.out.println("true1"+ calulatevalue(variable,variable3,variable0) );
+            calulatevalue(variable,variable3,variable0);
         } else if (variable2==2) {
             System.out.print ("true2");
             }
         else {
             System.out.println("true3");
         }
+
+    }
+    public static int calulatevalue( int cal1, boolean cal2, int variablecal){
+        int resultvariable=10;
+        if (variablecal==10){
+            resultvariable += 10;
+            System.out.println("cal_complete");
+
+        }
+        return resultvariable;
 
     }
 }
